6d525c6e54c3066da32dbd86ae4fafc1a455ba13
huangjus <huangjus@oregonstate.edu>
2023-01-10 15:19:20 -0800

    Initial commit

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Justin Huang
+# GitHub username: huangjus
+# Date: 1/10/23
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
